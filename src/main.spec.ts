0d2d8afa124446da936fb1d5c0dd65c369da8002
Marcin Mirecki <markky21@gmail.com>
2023-03-11 16:05:24 +0100

    chapter 2 end

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,7 +2,9 @@ import { Dollar } from "./main";
 
 describe("Dollar", () => {
   test("multiply", () => {
-    const sut = new Dollar(5);
-    expect(sut.multiply(2)).toEqual(10);
+    const dollar1 = new Dollar(5);
+    const dollar2 = dollar1.multiply(2);
+    expect(dollar2.amount).toEqual(10);
+    expect(dollar1.amount).toEqual(5);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 // 5 USD + 10CHF = 10 CHF if rate is 2:1
-// 5 USD * 2 = 10 USD
+// 5 USD * 2 = 10 USD - (DONE)
 // Field "amount" is private
-// What about side effects?
+// What about side effects? - (DONE)
 // What about rounding?
 
 export class Dollar {
   constructor(readonly amount: number) {}
 
   multiply(multiplier: number) {
-    return this.amount * multiplier;
+    return new Dollar(this.amount * multiplier);
   }
 }
