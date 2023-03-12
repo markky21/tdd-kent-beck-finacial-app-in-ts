4f82b761fbd0e0771c8d9eb4a7eda5d714d4cedc
Marcin Mirecki <markky21@gmail.com>
2023-03-12 10:16:49 +0100

    chapter 14 end

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,53 +1,56 @@
 import { Bank, Expression, Money, Sum } from "./main";
 
-describe("Money", () => {
-  test("equals", () => {
-    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
-    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
-    expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
-    expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
-    expect(Money.dollar(5).equals(Money.franc(5))).toBeFalsy();
-    expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
-  });
-
-  test("currency", () => {
-    expect(Money.dollar(1).currency).toEqual("USD");
-    expect(Money.franc(1).currency).toEqual("CHF");
-  });
-
-  test("multiply", () => {
-    const dollar1 = Money.dollar(5);
-    const dollar2 = dollar1.multiply(2);
-    expect(dollar2.amount).toEqual(10);
-    expect(dollar1.amount).toEqual(5);
-  });
-
-  test("addition the same currency", () => {
-    const dollar1 = Money.dollar(5);
-    const sum: Expression = dollar1.add(dollar1);
-    const bank = new Bank();
-    const reduced: Money = bank.reduce(sum, "USD");
-    expect(reduced.equals(Money.dollar(10))).toBeTruthy();
-  });
-
-  test("add return sum object instance", () => {
-    const dollar1 = Money.dollar(5);
-    const sum = dollar1.add(dollar1);
-    expect(dollar1.amount).toEqual(sum.augend.amount);
-  });
+test("equals", () => {
+  expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+  expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
+  expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
+  expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
+  expect(Money.dollar(5).equals(Money.franc(5))).toBeFalsy();
+  expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
 });
 
-describe("Bank", () => {
-  test("reduce sum", () => {
-    const sum = new Sum(Money.dollar(3), Money.dollar(4));
-    const bank = new Bank();
-    const result = bank.reduce(sum, "USD");
-    expect(result.equals(Money.dollar(7))).toBeTruthy();
-  });
+test("currency", () => {
+  expect(Money.dollar(1).currency).toEqual("USD");
+  expect(Money.franc(1).currency).toEqual("CHF");
+});
 
-  test("reduce money", () => {
-    const bank = new Bank();
-    const result = bank.reduce(Money.dollar(1), "USD");
-    expect(result.equals(Money.dollar(1))).toBeTruthy();
-  });
+test("multiply", () => {
+  const dollar1 = Money.dollar(5);
+  const dollar2 = dollar1.multiply(2);
+  expect(dollar2.amount).toEqual(10);
+  expect(dollar1.amount).toEqual(5);
+});
+
+test("addition the same currency", () => {
+  const dollar1 = Money.dollar(5);
+  const sum: Expression = dollar1.add(dollar1);
+  const bank = new Bank();
+  const reduced: Money = bank.reduce(sum, "USD");
+  expect(reduced.equals(Money.dollar(10))).toBeTruthy();
+});
+
+test("add return sum object instance", () => {
+  const dollar1 = Money.dollar(5);
+  const sum = dollar1.add(dollar1);
+  expect(dollar1.amount).toEqual(sum.augend.amount);
+});
+
+test("reduce sum", () => {
+  const sum = new Sum(Money.dollar(3), Money.dollar(4));
+  const bank = new Bank();
+  const result = bank.reduce(sum, "USD");
+  expect(result.equals(Money.dollar(7))).toBeTruthy();
+});
+
+test("reduce money", () => {
+  const bank = new Bank();
+  const result = bank.reduce(Money.dollar(1), "USD");
+  expect(result.equals(Money.dollar(1))).toBeTruthy();
+});
+
+test("reduce money different currency", () => {
+  const bank = new Bank();
+  bank.addRate("CHF", "USD", 2);
+  const result = bank.reduce(Money.franc(2), "USD");
+  expect(result.equals(Money.dollar(1))).toBeTruthy();
 });
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@
 // Compare Francs with Dollars - (DONE)
 // Currency? - (DONE)
 // Duplicate code in Dollar and Franc - (DONE)
-// 5 USD + 5 USD = 10 USD
+// Reduce (Bank, string) - (DONE)
+// Reduce Money with conversion - (DONE)
+// 5 USD + 5 USD = 10 USD - (DONE)
+
 // 5 USD + 10 CHF = 20 USD if rate is USD:CHF=2:1
 // What about rounding?
 // Method hashCode()
@@ -14,19 +17,19 @@
 // Compare to null
 // Common multiplication
 // Floating point numbers in JS :)
-// Reduce Money with conversion
-// Reduce (Bank, string)
+// Money object as result of addition 5 USD + 5 USD
 
 type Currency = "USD" | "CHF";
+type HashCode = string;
 
 export interface Expression {
-  reduce(to: Currency): Money;
+  reduce(bank: Bank, to: Currency): Money;
 }
 
 export class Sum implements Expression {
   constructor(readonly augend: Money, readonly addend: Money) {}
 
-  reduce(): Money {
+  reduce(bank: Bank, to: Currency): Money {
     return new Money(
       this.augend.currency,
       this.augend.amount + this.addend.amount
@@ -34,10 +37,33 @@ export class Sum implements Expression {
   }
 }
 
+export class Pair {
+  constructor(readonly from: Currency, readonly to: Currency) {}
+
+  equals(object: Pair): boolean {
+    return this.from === object.from && this.to === object.to;
+  }
+
+  hashCode(): HashCode {
+    return `${this.from}_${this.to}`;
+  }
+}
+
 export class Bank {
   reduce(source: Expression, to: Currency): Money {
-    return source.reduce(to);
+    return source.reduce(this, to);
   }
+
+  addRate(from: Currency, to: Currency, rate: number) {
+    this.rates.set(new Pair(from, to).hashCode(), rate);
+  }
+
+  rate(from: Currency, to: Currency): number {
+    if (from === to) return 1;
+    return this.rates.get(new Pair(from, to).hashCode()) || 0;
+  }
+
+  private rates: Map<HashCode, number> = new Map();
 }
 
 export class Money implements Expression {
@@ -70,8 +96,9 @@ export class Money implements Expression {
     return new Sum(this, addend);
   }
 
-  reduce(to: Currency): Money {
-    return this;
+  reduce(bank: Bank, to: Currency): Money {
+    const rate = bank.rate(this.currency, to);
+    return new Money(to, this.amount / rate);
   }
 
   static dollar(amount: number): Money {
